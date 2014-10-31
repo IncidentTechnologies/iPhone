42703de0ab12361cefb0c1ef6ca4bb3e62634194
Idan Beck <idanbeck@incidenttech.com>
2014-10-31 15:12:37 -0700

    gTar Create stuff for piezo

--- gTarCreate/gTarCreate/GtarViewController.h
+++ gTarCreate/gTarCreate/GtarViewController.h
@@ -9,16 +9,21 @@
 #import <UIKit/UIKit.h>
 #import "GtarControllerInternal.h"
 
-@interface GtarViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, GtarControllerDelegate>
-{
+@interface GtarViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, GtarControllerDelegate> {
     NSArray *m_firmwares;
     UIView *m_disableView;
+    
+    NSArray *m_piezoFirmwares;
 }
 
 @property (nonatomic, retain) IBOutlet UIButton *m_buttonFWUpgrade;
 @property (nonatomic, retain) IBOutlet UITableView *m_tableViewFirmwares;
 
+@property (nonatomic, retain) IBOutlet UIButton *m_buttonPiezoFWUpgrade;
+@property (nonatomic, retain) IBOutlet UITableView *m_tableViewPiezoFirmwares;
+
 -(IBAction)OnFWUpgradeClick:(id)sender;
+-(IBAction)OnPiezoFWUpgradeClick:(id)sender;
 
 - (void)receivedFirmwareUpdateProgress:(unsigned char)percentage;
 - (void)receivedFirmwareUpdateStatusSucceeded;
